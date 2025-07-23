899776110d76f51f0403b979d9fada088b07e20c
Carmelo Es <carmelo.escribano@gmail.com>
2025-07-23 18:54:22 +0200

    primera versión

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 urlpatterns = [
 path('', views.ProductoListView.as_view(), name='lista_productos'),
+path('categorias/', views.CategoriaListView.as_view(), name='lista_categorias'),
 path('nuevo/', views.ProductoCreateView.as_view(), name='crear_producto'),
+path('nuevo_categoria/', views.CategoriaCreateView.as_view(), name='crear_categoria'),
 path('editar/<int:pk>/', views.ProductoUpdateView.as_view(), name='editar_producto'),
+path('editar_categoria/<int:pk>/', views.CategoriaUpdateView.as_view(), name='editar_categoria'),
 path('eliminar/<int:pk>/', views.ProductoDeleteView.as_view(), name='eliminar_producto'),
+path('eliminar_categoria/<int:pk>/', views.CategoriaDeleteView.as_view(), name='eliminar_categoria'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,18 +1,26 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from .models import  Producto
+from .models import Producto, Categoria
 from django.urls import reverse_lazy
 
 class ProductoListView(ListView):
     model = Producto
     template_name = 'app/lista.html'
 
+class CategoriaListView(ListView):
+    model = Categoria
+    template_name = 'app/lista_categorias.html'
 class ProductoCreateView(CreateView):
     model = Producto
     fields = ['nombre', 'unidades','precio','categoria']
     template_name = 'app/formulario.html'
     success_url = reverse_lazy('lista_productos')
 
+class CategoriaCreateView(CreateView):
+    model = Categoria
+    fields = ['nombre', 'descripcion']
+    template_name = 'app/formulario_categorias.html'
+    success_url = reverse_lazy('lista_categorias')
 
 class ProductoUpdateView(UpdateView, LoginRequiredMixin):
     model = Producto
@@ -20,7 +28,17 @@ class ProductoUpdateView(UpdateView, LoginRequiredMixin):
     template_name = 'app/formulario.html'
     success_url = reverse_lazy('lista_productos')
 
+class CategoriaUpdateView(UpdateView, LoginRequiredMixin):
+    model = Categoria
+    fields = ['nombre', 'descripcion']
+    template_name = 'app/formulario_categorias.html'
+    success_url = reverse_lazy('lista_categorias')
 class ProductoDeleteView(DeleteView):
     model = Producto
     template_name = 'app/eliminar.html'
     success_url = reverse_lazy('lista_productos')
+
+class CategoriaDeleteView(DeleteView):
+    model = Categoria
+    template_name = 'app/eliminar_categoria.html'
+    success_url = reverse_lazy('lista_categorias')
